8eb9f926eec64b8f590db6c08e49f954707dfb1f
Emir-AL1 <ibragimovemir999@gmail.com>
2026-01-03 21:08:29 +0300

    tshirts_10_6

--- routes/index.js
+++ routes/index.js
@@ -37,13 +37,6 @@ router.post('/logreg', async function(req, res, next) {
 
   }  
 });
-/* POST logout. */
-router.post('/logout', function(req, res, next) {
- req.session.destroy();
- res.locals.user = null;
- res.redirect('/');
-});
-
 
 
 
