8fac176aa6c330e864f53841a543e437f6c823d1
Emir-AL1 <ibragimovemir999@gmail.com>
2026-01-03 20:08:59 +0300

    tshirts_8_2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/tshirts2025')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var indexTshirts = require('./routes/tshirts');
 var app = express();
 
 // view engine setup
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/tshirts', indexTshirts);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- testMongoose.js
+++ testMongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2025');
 
 
-var Tshirts = require('./models/tshirts.js').Tshirts
+var Tshirts = require('./models/tshirt.js').Tshirts
 
 
 
