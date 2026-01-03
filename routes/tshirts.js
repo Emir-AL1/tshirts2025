d987178241c38373e613647bbb7b1ac04dc4015e
Emir-AL1 <ibragimovemir999@gmail.com>
2026-01-03 21:13:26 +0300

    tshirts_10_8

--- routes/tshirts.js
+++ routes/tshirts.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var Tshirts = require('../models/tshirt').Tshirts;
+var checkAuth = require("../middlewares/checkAuth.js");
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с tshirts');
 });
 /* Страница футболок*/
-router.get("/:nick", async function(req, res, next) {
+router.get("/:nick", checkAuth, async function(req, res, next) {
+
    var tshirts = await Tshirts.find({nick: req.params.nick});
    console.log(tshirts)
    if(!tshirts.length) return next(new Error("Нет такой футболки в наличии"))
