373e0c50219a30ac575aae41d30f01321d1ebcc3
Emir-AL1 <ibragimovemir999@gmail.com>
2026-01-03 19:54:23 +0300

    tshirts_7_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2025');
 
-var schema = mongoose.Schema({ name: String })
-const shirt = mongoose.model('Tshirt', { name: String });
+const shirtSchema = new mongoose.Schema({ 
+  name: String 
+});
 
+shirtSchema.methods.show = function () {
+  console.log(`${this.name} — в магазине прямо сейчас!`);
+};
 
-const puma = new shirt ({ name: 'puma' });
-puma.save().then(() => console.log('Футболка готова)'));
+const Shirt = mongoose.model('Shirts', shirtSchema);
 
+const puma = new Shirt({ name: 'puma' });
+
+puma.save().then(() => {
+  console.log('Кроссовок успешно сохранён!');
+  puma.show();  // ← вот здесь сработает наш метод
+});
