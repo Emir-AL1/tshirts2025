9e668094d19e8d5bc9781a4bb1f89fa8b4c40c7c
Emir-AL1 <ibragimovemir999@gmail.com>
2026-01-03 18:44:36 +0300

    tshirts_4_4

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('ejs',require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -33,9 +34,8 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+ // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { title: 'Three Tshirts' });
 });
-
 module.exports = app;
