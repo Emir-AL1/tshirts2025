c14fec8c9dd0a3d8f0a916aa4bca2a201bb8bec8
Emir-AL1 <ibragimovemir999@gmail.com>
2026-01-03 20:24:56 +0300

    tshirts_9_4

--- routes/index.js
+++ routes/index.js
@@ -3,37 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
- req.session.greeting = "Hi!!!";
- res.render('index', { title: 'Express' });
-});
-
-
-/* Страница maison */
-router.get('/maison', function(req, res, next) {
-    res.render('tshirt', {
-        title: "Maison Margiela",
-        picture: "images/maison.jpg",
-        desc: "Футболка Maison Margiela отличается авангардным дизайном, минимализмом, премиальными материалами, аккуратным кроем, подчёркивая индивидуальность, стиль, концептуальность, современную моду высокого класса люкс."
-    });
-});
-
-
-/* Страница puma */
-router.get('/puma', function(req, res, next) {
-    res.render('tshirt', {
-        title: "Puma",
-        picture: "images/puma.jpg",
-        desc: "Футболка Puma сочетает спортивный стиль, комфорт, дышащие ткани, удобную посадку, динамичный дизайн, подходящий для тренировок, активного отдыха и повседневной носки."
-    });
-});
-
-/* Страница stussy */
-router.get('/stussy', function(req, res, next) {
-    res.render('tshirt', {
-        title: "Stussy",
-        picture: "images/stussy.jpg",
-        desc: "Футболка Stussy отражает уличную культуру, свободный крой, графические принты, калифорнийский вайб, комфорт, самовыражение, актуальность, молодежный стиль, тренды, свободу движения сегодня."
-    });
+    res.render('index', { title: 'Express',  counter:req.session.counter });
 });
 
 
